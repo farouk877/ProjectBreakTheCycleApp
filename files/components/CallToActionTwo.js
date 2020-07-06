1dace4527725dd37b657bcbc157f48876c3e1396
farouk877 <faroukmarhaba1@gmail.com>
2020-07-06 02:56:57 -0400

    added in calltoaction2

--- files/components/CallToActionTwo.js
+++ files/components/CallToActionTwo.js
@@ -5,16 +5,16 @@ const CallToActionTwo = () => {
         <section className="cta-area text-center">
             <div className="container-fluid">
                 <div className="row">
-                    {/* <div className="col-lg-6 col-sm-6">
-                        <div className="cta-item cta-item1">
-                            <h3 className="cta__title">Sponsor an entire campaign</h3>
-                            <a href="/sponsorship" className="theme-btn">become an sponsor</a>
-                        </div>
-                    </div> */}
                     <div className="col-lg-6 col-sm-6">
                         <div className="cta-item cta-item1">
                             <h3 className="cta__title">Dedicate monthly contributions</h3>
                             <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SKD88K8R387KQ&source=url" className="theme-btn">Donate</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-sm-6">
+                        <div className="cta-item cta-item1">
+                            <h3 className="cta__title">Buy from our Shop - 100% profits donated</h3>
+                            <a href="/shop" className="theme-btn">shop</a>
                         </div>
                     </div>
                 </div>
