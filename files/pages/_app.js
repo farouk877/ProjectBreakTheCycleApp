9e7c25a61f5770b39896e745b3f282bc8daf8db7
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-07-13 22:23:53 -0400

    loading bar for website

--- files/pages/_app.js
+++ files/pages/_app.js
@@ -1,19 +1,45 @@
-import '../node_modules/react-modal-video/scss/modal-video.scss';
-import * as gtag from '../util/gtag';
-import { useEffect } from 'react';
+// import '../node_modules/react-modal-video/scss/modal-video.scss';
+// import * as gtag from '../util/gtag';
+// import { useEffect } from 'react';
+// import Router from 'next/router';
+// import NProgress from 'nprogress';
+// import getConfig from 'next/config';
+
+
+
+
+// // This default export is required in a new `pages/_app.js` file.
+// export default function MyApp({ Component, pageProps }) {
+//     useEffect(() => {
+//         const handleRouteChange = (url) => {
+//           gtag.pageview(url)
+//         }
+//         Router.events.on('routeChangeComplete', handleRouteChange)
+
+//         return () => {
+//           Router.events.off('routeChangeComplete', handleRouteChange)
+   
+//         }
+//       }, []);
+//     return <Component {...pageProps} />
+// }
+
+
 import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+import * as gtag from '../util/gtag';
 
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-          gtag.pageview(url)
-        }
-        Router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-          Router.events.off('routeChangeComplete', handleRouteChange)
-        }
-      }, []);
+function MyApp({ Component, pageProps }) {
+    
+  const handleRouteChange = (url) => {
+    gtag.pageview(url)
+  }
+    
+
     return <Component {...pageProps} />
 }
+export default MyApp;
