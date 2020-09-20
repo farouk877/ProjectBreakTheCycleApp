499ee7862aae2a83fda5a14e646f4d6fe521e6d4
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-09-20 16:32:57 -0400

    updated donation fix

--- files/components/LebanonReliefFund.js
+++ files/components/LebanonReliefFund.js
@@ -19,7 +19,7 @@ const LebanonReliefFund = () => {
                                         <h3 className="blog__title">Lebanon Relief Fund</h3>
                                         <ul className="blog__list">
                                             <li><i className="icon-target"></i> Goal: <span>$10,000</span></li>
-                                            {/* <li><i className="fa fa-line-chart"></i> Raised: <span>$320</span></li> */}
+                                            <li><i className="fa fa-line-chart"></i> Raised: <span>$320</span></li>
                                         </ul>
                                     </div>
                                     <div className="inner-causes-btn">
