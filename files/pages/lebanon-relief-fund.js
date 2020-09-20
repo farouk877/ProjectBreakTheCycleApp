9f75ae6ca8c7e6d1bf1b83608b6aa7df27aa5020
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-09-20 19:13:08 -0400

    fix lebanon page

--- files/pages/lebanon-relief-fund.js
+++ files/pages/lebanon-relief-fund.js
@@ -6,28 +6,16 @@ import Footer from "../components/Footer";
 import CallToAction from '../components/CallToAction';
 import LebanonReliefFund from '../components/LebanonReliefFund';
 
-// const LebanonRelief = () => {
-//     // window.location = "https://www.gofundme.com/f/local-emergency-relief-fund-for-lebanon";
-//     // return (
-//     //     <Layout pageTitle="PBTC | Lebanon Relief Fund">
-//     //         <NavOne />
-//     //         <PageHeader title="Lebanon Relief Fund" />
-//     //         <LebanonReliefFund />
-//     //         <CallToAction />
-//     //         <Footer />
-//     //     </Layout>
-//     // );
-//     return(
-//         location.replace("https://www.w3schools.com")
+const LebanonRelief = () => {
+    return (
+        <Layout pageTitle="PBTC | Lebanon Relief Fund">
+            <NavOne />
+            <PageHeader title="Lebanon Relief Fund" />
+            <LebanonReliefFund />
+            <CallToAction />
+            <Footer />
+        </Layout>
+    );
+};
 
-
-//     );
-// };
-
-// export default LebanonRelief;
-
-window.location.replace("https://www.w3schools.com")
-
-export default function LebanonRelief() {
-  return null;
-}
+export default LebanonRelief;
