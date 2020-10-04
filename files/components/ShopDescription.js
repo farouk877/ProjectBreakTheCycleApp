fe72c12239954fbe5f2327296edc5dc3871937aa
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-10-04 11:44:26 -0400

    shop description update

--- files/components/ShopDescription.js
+++ files/components/ShopDescription.js
@@ -16,9 +16,10 @@ const ShopDescription = () => {
                                         Welcome to our shop! As always, 100% of profits go towards funding the current campaign! 
                                         Learn more about our campaigns by visiting our Campaigns page. Your purchase directly 
                                         impacts our mission to implement sustainable ways of breaking the cycle of injustice around 
-                                        the world. Please allow 1-3 weeks to receive your order, as all items are made-to-order.
+                                        the world. Please allow 1-3 weeks to receive your order. We keep a limited stock of our designs
+                                        so don't wait to buy cause once we run out we don't restock.
                                         <br/><br/>
-                                        Our Shop items will be releasing on July 18, 2020. Follow us on our social media to stay updated!
+                                        Our Shop items are available now! Follow us on our social media to stay updated!
                                     </p>
                                 </div>                              
                             </div>
